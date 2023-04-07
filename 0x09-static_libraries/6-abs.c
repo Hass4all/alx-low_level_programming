31cb56fb73795deb18f275ee2a047e4d847193d6
hass4all <hass4all@hotmail.com>
2023-04-07 16:59:25 -0700

    .

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,21 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _abs - Entry point
- * Description - Print absolute value
- * @n: Integer of Abs Value
- * Return: int
+ * _abs - value absolute
+ *
+ *@n: The int to print
+ * Return: Always 0.
  */
 
 int _abs(int n)
 {
-	if (n < 0)
-	{
-		return (-n);
-	}
-	else
-	{
-		return (n);
-	}
+
+if (n < 0)
+{
+return (n * (-1));
+}
+
+else if (n == 0)
+{
+return (0);
+}
+
+else
+{
+return (n);
+}
+
 }
 
